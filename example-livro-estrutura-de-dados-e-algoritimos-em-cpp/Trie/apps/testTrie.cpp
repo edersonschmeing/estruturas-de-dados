55cec0b3665c682667505bc0ee1b40c690622516
edersonschmeing <edersonschmeing@gmail.com>
2019-08-20 12:45:33 -0300

    add trie

---
+++ example-livro-estrutura-de-dados-e-algoritimos-em-cpp/Trie/apps/testTrie.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "../include/trie.h"
+
+int main() {
+     
+   Trie trie;
+
+  /* puts("add to head 9 8 7");
+   trie.addToHead(9);
+   trie.addToHead(8);
+   trie.addToHead(7);
+   trie.printAll();
+   
+   puts("add to tail 4 5 6");
+   trie.addToTail(4);
+   trie.addToTail(5);
+   trie.addToTail(6);
+   trie.printAll();
+  
+   puts("Delete head");
+   trie.deleteFromHead();
+  
+   puts("Delete tail");
+   trie.deleteFromTail();
+   trie.printAll(); */
+
+   return 0;
+}
